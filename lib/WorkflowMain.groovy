dc49d62878537f6511361d952d68036a8dbf62bf
Luan Nguyen <l.nguyen@hartwigmedicalfoundation.nl>
2025-02-18 15:32:19 +1100

    Include BWA index when preparing panel reference data

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -353,6 +353,7 @@ class WorkflowMain {
 
         if (ref_data_types.contains(Constants.RefDataType.PANEL)) {
             require_genome = true
+            require_bwa_index = true
             require_hmftools_data = true
         }
 
