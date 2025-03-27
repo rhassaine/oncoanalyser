dd592c9dc41044b4463a75ed7b078860d4f6bf4d
rhassaine <rhassaine@hartwigmedicalfoundation.nl>
2025-03-27 16:30:18 +0100

    Solved merge conflicts

--- lib/Constants.groovy
+++ lib/Constants.groovy
@@ -26,10 +26,7 @@ class Constants {
 
 
     static enum RunMode {
-<<<<<<< HEAD
-=======
         PURITY_ESTIMATE,
->>>>>>> 964faa0... Rename mrd mode to purity_estimate
         TARGETED,
         WGTS,
     }
